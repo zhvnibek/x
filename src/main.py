ce55417f54f22972c346d59fb934fe20068a431e
zhanibek <zh.darimbek@gmail.com>
2023-03-18 21:53:00 +0600

    test on streamlit

--- src/main.py
+++ src/main.py
@@ -22,15 +22,14 @@ st.sidebar.title(f"Victara AI")
 
 st.title('Show me your face!')
 
-picture = st.camera_input("First, take a picture...")
-
 image_filepath = 'face.jpg'
+bg_image = None
 
+picture = st.camera_input("First, take a picture...")
 if picture:
-    with open(image_filepath, "wb") as f:
+    with open(image_filepath, "wb+") as f:
         f.write(picture.read())
-
-bg_image = Image.open(image_filepath)
+    bg_image = Image.open(image_filepath)
 
 
 def make_mask(mask_paths):
@@ -78,65 +77,66 @@ def make_noise():
     return noise
 
 
-canvas_result = st_canvas(
-    stroke_color="yellow",
-    stroke_width=3,
-    background_image=bg_image,
-    height=512,
-    width=512,
-    # height=bg_image.height,
-    # width=bg_image.width,
-    drawing_mode="freedraw",
-    key="compute_arc_length",
-)
-if canvas_result.json_data is not None and canvas_result.json_data["objects"]:
-    sketch_paths = []
-
-    paths = pd.json_normalize(canvas_result.json_data["objects"])["path"].tolist()
-    for ind, path in enumerate(paths):
-        path = svgpathtools.parse_path(" ".join([str(e) for line in path for e in line]))
-        sketch_paths.append([
-            (int(path.point(pos).real), int(path.point(pos).imag))
-            for pos in np.linspace(0, 1, int(path.length()))
-        ])
-
-    # prepare original input image as tensor
-    mat_img = cv2.imread(image_filepath)
-    mat_img = cv2.resize(mat_img, (512, 512), interpolation=cv2.INTER_CUBIC)
-    mat_img = mat_img / 127.5 - 1
-    mat_img = np.expand_dims(mat_img, axis=0)
-
-    # prepare sketch, color, mask, noise as tensors
-    sketch = make_sketch(sketch_paths=sketch_paths)
-    color = make_stroke(stroke_paths=sketch_paths)
-    mask = make_mask(mask_paths=sketch_paths)
-
-    noise = make_noise()
-
-    sketch = sketch * mask
-    color = color * mask
-    noise = noise * mask
-
-    # concatenate all tensors as a single input tensor for FEGAN model
-    batch = np.concatenate(
-        [
-            mat_img,  # original image, 0:3
-            sketch,  # sketch, 3:4
-            color,  # color, 4:7
-            mask,  # mask, 7:8
-            noise  # noise, 8:9
-        ],
-        axis=3
+if bg_image:
+    canvas_result = st_canvas(
+        stroke_color="yellow",
+        stroke_width=3,
+        background_image=bg_image,
+        height=512,
+        width=512,
+        # height=bg_image.height,
+        # width=bg_image.width,
+        drawing_mode="freedraw",
+        key="compute_arc_length",
     )
-    print(batch.shape)
+    if canvas_result.json_data is not None and canvas_result.json_data["objects"]:
+        sketch_paths = []
 
-    fegan = get_model()
-    result = fegan.demo(batch)
-    result = (result + 1) * 127.5
-    output_img = np.asarray(result[0, :, :, :], dtype=np.uint8)
-    print(output_img.shape)
-    cv2.imwrite('output.jpg', output_img)
-    st.image(output_img, channels='BGR')
+        paths = pd.json_normalize(canvas_result.json_data["objects"])["path"].tolist()
+        for ind, path in enumerate(paths):
+            path = svgpathtools.parse_path(" ".join([str(e) for line in path for e in line]))
+            sketch_paths.append([
+                (int(path.point(pos).real), int(path.point(pos).imag))
+                for pos in np.linspace(0, 1, int(path.length()))
+            ])
 
-    # st.image(sketch, clamp=True, channels='BGR')
-    # st.image(mask, clamp=True, channels='BGR')
+        # prepare original input image as tensor
+        mat_img = cv2.imread(image_filepath)
+        mat_img = cv2.resize(mat_img, (512, 512), interpolation=cv2.INTER_CUBIC)
+        mat_img = mat_img / 127.5 - 1
+        mat_img = np.expand_dims(mat_img, axis=0)
+
+        # prepare sketch, color, mask, noise as tensors
+        sketch = make_sketch(sketch_paths=sketch_paths)
+        color = make_stroke(stroke_paths=sketch_paths)
+        mask = make_mask(mask_paths=sketch_paths)
+
+        noise = make_noise()
+
+        sketch = sketch * mask
+        color = color * mask
+        noise = noise * mask
+
+        # concatenate all tensors as a single input tensor for FEGAN model
+        batch = np.concatenate(
+            [
+                mat_img,  # original image, 0:3
+                sketch,  # sketch, 3:4
+                color,  # color, 4:7
+                mask,  # mask, 7:8
+                noise  # noise, 8:9
+            ],
+            axis=3
+        )
+        print(batch.shape)
+
+        fegan = get_model()
+        result = fegan.demo(batch)
+        result = (result + 1) * 127.5
+        output_img = np.asarray(result[0, :, :, :], dtype=np.uint8)
+        print(output_img.shape)
+        cv2.imwrite('output.jpg', output_img)
+        st.image(output_img, channels='BGR')
+
+        # st.image(sketch, clamp=True, channels='BGR')
+        # st.image(mask, clamp=True, channels='BGR')
